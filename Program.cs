254e366b0495442e9e49f312eb3ac760eecd4c27
lucasmenchon <it.lucas@outlook.com>
2022-05-19 13:46:44 -0300

    adicionado novos metodos envio api

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
@@ -11,6 +13,21 @@ response.Headers.Add("Header", "Lucas Menchon");
 //Api Post Produto
 app.MapPost("/saveproduct", (Product product ) =>  {
 return product.Code + " - " + product.Name;
+});
+
+//api.app.com/users?datastart={date}&dataend={date} < "exemplo enviar relatorio" > via query string
+app.MapGet("/getproduct", ([FromQuery]string dateStart, string dateEnd) => {
+    return dateStart + " - " + dateEnd;
+});
+
+//api.app.com/user/{code} < via rota "pertence a x endpoint" < e obrigatorio
+app.MapGet("/getproduct/{code}", ([FromRoute]string code) => {
+    return code;
+});
+
+//atraves do header solicitante
+app.MapGet("/getproductheader", (HttpRequest request) => { 
+    return request.Headers["product-code"].ToString();
 });
 
 app.Run();
